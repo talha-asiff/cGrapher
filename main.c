e64c7b9af1299018dab4f529c3700154a6c919b9
MUHAMMAD TALHA ASIF <sp26-bse-064@cuilahore.edu.pk>
2026-04-24 10:51:12 +0500

    Refactor main.c to improve point management

--- main.c
+++ main.c
@@ -3,12 +3,81 @@
 #include<conio.h>
 #define UP 72
 #define DOWN 80
+#define select 13
+
+int size = 0;
+int * x = NULL;
+int * y = NULL;
+char padd[50] = "";
+void graph(){
+	x = malloc(size * sizeof(int));
+	y = malloc(size * sizeof(int));
+	int i, j;
+	char ch = 'A';
+	int count = 0;
+	char in;
+	start:
+		count++;
+		padd[count-1] = ch;
+		system("cls");
+		printf("50x50\n");
+		for(i = -26; i <= 26; i++){
+			for(j = -26; j<=26; j++){
+				if(i==0 || j==0){
+					printf("#");
+				}
+				else{
+					if(i==x[count-1] && j==y[count-1]){
+						printf(ch);
+					}
+					else{
+						printf(".");
+					}
+				}
+			}
+			printf("\n");
+		}
+		i = 0;
+		j = 0;
+		printf("Enter 0 to add new point or enter any character to exit\n");
+		in = getch();
+		if(in == '0'){
+			int a, b;
+			x = realloc(x, (size++)*sizeof(int));
+			y = realloc(y, (size++)*sizeof(int));
+			printf("Enter Points: \n");
+			printf("x : ");
+			scanf("%d", &a);
+			printf("y : ");
+			scanf("%d", &b);
+			x[count-1] = a;
+			y[count-1] = b;
+			goto start;
+		}
+		else{
+			system("cls");
+		}
+	
+}
+double distance(){
+	
+}
+double angle(){
+	
+}
+double slope(){
+	
+}
+void export(){
+	
+}
+
 int main(){
 	int pointer = 0;
 	char in;
-	bool selected = false;
+	int selected = 0;
 	while(1){
-	
+		start:
 		printf("  ____  ____                _               \n");
     	printf(" / ___|/ ___|_ __ __ _ _ __ | |__   ___ _ __ \n");
 	    printf("| |     |  _| '__/ _` | '_ \\| '_ \\ / _ \\ '__|\n");
@@ -71,8 +140,8 @@ int main(){
 		if(in == DOWN){
 			pointer++;
 		}
-		if(in != UP && in != DOWN){
-			selected = true;
+		if(in == select){
+			selected = 1;
 		}
 		if(pointer == 6){
 			pointer = 0;
@@ -81,5 +150,30 @@ int main(){
 			pointer = 5;
 		}
 		system("cls");
+		if(selected){
+			if(pointer == 0){
+				goto o1;
+			}
+			else if(pointer == 1){
+				goto o2;
+			}
+			else if(pointer == 2){
+				goto o3;
+			}
+			else if(pointer == 3){
+				goto o4;
+			}
+			else if(pointer == 5){
+				break;
+			}
+		}
 	}
+	if(0){
+		o1: graph();
+		o2: distance();
+		o3: angle();
+		o4: slope();
+		goto start;
+	}
+	
 }
