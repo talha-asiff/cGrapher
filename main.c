bf0b60577ff1d55082414eeabd7ff4ca9b701664
MUHAMMAD TALHA ASIF <sp26-bse-064@cuilahore.edu.pk>
2026-04-25 15:43:44 +0500

    Refactor graph function and improve clarity

--- main.c
+++ main.c
@@ -6,76 +6,65 @@
 #define UP 72
 #define DOWN 80
 #define select 13
-// ye global variables hein jin sy program ke different functions me ham points ke values pass kry gy
-int size = 1;		// ye dynamic var ke lye bnaya ha agy smj a jay ge
-int *x = NULL;		// ham ny yaha memory heap sy x aur y lye hein
-int *y = NULL;		// ye var garbage value na de is lye hm ny pehly he NULL kr dia ha safe side rehny ke lye
-char padd[50] = ""; // empty string jo points ke names store kry ga, jese A, B, C etc
+int size = 1;		
+int *x = NULL;		
+int *y = NULL;		
+char padd[50] = ""; 
 void graph()
 {
-	system("cls");						   // graph fucn ke start me screen clear kr do ga, taki graph ke sath menu ka text mix na ho
-	int a, b;							   // ye temporary point ke coordiantes store krny ke lye hain, jese user jab new point add kray ga to uske x aur y coordinates ko store krny ke lye
-	x = (int *)malloc(size * sizeof(int)); // ab x ptr me memory allocate ke 1*4 = 4 bytes allocate ho jay ge, aur y ptr me bhi 4 bytes allocate ho jay ge, kyun ke size ki value 1 ha, aur int ka size 4 bytes hota ha
-	y = (int *)malloc(size * sizeof(int)); // ye dekho
-	int i, j;							   // counter variables for loops
-	char ch = 'A';						   // ye point ke lye name rakha ha A
-	int count = 0;						   // ye variable count krta rahe ga ke kitne points add ho chuke hein, taki me uss hisab sy x aur y arrays me values store kr saku, aur padd string me point ke names store kr saku
+	system("cls");	
+	int a, b;							   
+	x = (int *)malloc(size * sizeof(int)); 
+	y = (int *)malloc(size * sizeof(int)); 
+	int i, j;							   
+	char ch = 'A';						   
+	int count = 0;						   
 	char in;
 	while (1)
 	{
-		//	system("cls");
 		printf("Point : %c\n", ch);
-		// graph:
-		for (i = 26; i >= -26; i--)
-		{ // ye graph ke logic to btae thee na pehly bhe
-			for (j = -26; j <= 26; j++)
-			{
-				if (i == 0 || j == 0)
-				{
+		for (i = 26; i >= -26; i--){ 
+			for (j = -26; j <= 26; j++){
+				if (i == 0 || j == 0){
 					printf("#");
 				}
-				else
-				{
-					if (i == b && j == a)
-					{
+				else{
+					if (i == b && j == a){
 						printf("#");
 					}
-					else
-					{
+					else{
 						printf(".");
 					}
 				}
 			}
 			printf("\n");
 		}
-		i = 0; // set i and j back to zero for the next loop, jese user jab new point add kray ga to graph dobara draw ho jay ga, aur usme naye point ke sath purane points bhi show ho jay ge
+		i = 0; 
 		j = 0;
-		// in wala jo var tha upr us me ek character enter kry ga user, agr wo 0 hua to program
 		printf("Enter 0 to add new point or enter any character to exit\n");
 		in = _getch();
 		if (in == '0')
 		{
-			// ab jb user 0 enter kry ga new point ke lye to
-			x = realloc(x, (size) * sizeof(int)); // yaha size = 1 ha dubara sy realloc x ka size 4 kr de ga, next time size = 2 ho jay ga aur realloc 2*4 kr ky x ko 8 bytes de de ga.........and so on
-			y = realloc(y, (size) * sizeof(int)); // same for y
+			x = realloc(x, (size) * sizeof(int));
+			y = realloc(y, (size) * sizeof(int));
 			printf("Enter Points: \n");
 			printf("x : ");
-			scanf("%d", &a); // yaha a aur b me temporary point ke coordinates store ho jay ge, jese user jab new point add kray ga to uske x aur y coordinates ko store krny ke lye
+			scanf("%d", &a);
 			printf("y : ");
 			scanf("%d", &b);
-			x[count] = a; // ab upr count = 0 tha, x[0] me a ke value save kr de ga
+			x[count] = a;
 			y[count] = b;
 			system("cls");
-			padd[count] = ch; // padd[0] me ch = A rakh de ga
+			padd[count] = ch;
 			ch++;
 			count++;
 			size++;
 		}
 		else
 		{
-			system("cls"); // agr user 0 ke ilawa koi character enter kry ga to screen clear kr do ga, taki menu ke sath graph ka text mix na ho, aur
-			break;		   // loop sy exit kr de ga
-			return;		   // aur phir function leave kr de ga
+			system("cls");
+			break;
+			return;
 		}
 	}
 }
@@ -241,7 +230,7 @@ start:
 		}
 		if (pointer == 6)
 		{
-			pointer = 0; // ye condition ha ke agr pointer variable ki value 6 ho jayegi, to me uss variable ki value 0 kr do ga, taki menu me wapas se pehla option pr chala jao
+			pointer = 0;
 		}
 		if (pointer == -1)
 		{
@@ -271,14 +260,14 @@ start:
 				break;
 			}
 		}
-	} // ye sary labels aur unky agy functions me ny ese condition me rakh dye hein jo hamesha false rhy ge
+	}
 	if (0)
-	{ // always false condition
+	{
 	o1:
-		graph();	 // compiler yaha ay ga agr user ny 0 point pr enter press kia
-		pointer = 0; // graph func khatam hone ke baad me pointer variable ki value 0 kr do ga, taki menu me wapas se pehla option pr chala jao
+		graph();
+		pointer = 0;
 		selected = 0;
-		goto start; // ye phir start pr dubara jump kr de ga (inside the loop)
+		goto start;
 
 	o2:
 		distance();
